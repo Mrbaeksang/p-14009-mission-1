ad969c2787e4a4ee3d84a0725d535501164def4c
Mrbaeksang <qortkdgus95@gmail.com>
2025-05-23 15:17:54 +0900

    방식 재구성

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -14,89 +14,80 @@ public class Main {
 
         while (true) {
             System.out.print("명령) ");
-            String command = sc.nextLine();
+            String command = sc.nextLine().trim();
 
-            switch (command) {
-                case "등록":
-                    System.out.print("명언을 입력하세요: ");
-                    String content = sc.nextLine();
+            if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String content = sc.nextLine();
 
-                    System.out.print("작가를 입력하세요: ");
-                    String author = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
 
-                    quotes.add(new Quote(id, content, author));
+                quotes.add(new Quote(id, content, author));
 
-                    System.out.println("명언 : " + content);
-                    System.out.println("작가 : " + author);
-                    System.out.println(id + "번 명언이 등록되었습니다.");
-                    id++;
-                    break;
-
-                case "목록":
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("--------------------");
-                    for (Quote quote : quotes) {
-                        System.out.println(quote.id + " / " + quote.author + " / " + quote.content);
-                    }
-                    break;
-
-                case "삭제":
-                    System.out.println("삭제할 번호를 입력하세요: ");
-                    int deleteId = sc.nextInt();
-                    sc.nextLine();
-
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+                id++;
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = quotes.size() - 1; i >= 0; i--) {
+                    Quote quote = quotes.get(i);
+                    System.out.printf("%d / %s / %s\n", quote.id, quote.author, quote.content);
+                }
+            } else if (command.startsWith("삭제?id=")) {
+                try {
+                    int deleteId = Integer.parseInt(command.substring("삭제?id=".length()));
+                    
                     boolean found = false;
                     for (int i = 0; i < quotes.size(); i++) {
                         if (quotes.get(i).id == deleteId) {
                             quotes.remove(i);
-                            System.out.println(deleteId + "번 명언이 삭제되었습니다.");
+                            System.out.println("%d번 명언이 삭제되었습니다.".formatted(deleteId));
                             found = true;
                             break;
                         }
                     }
 
                     if (!found) {
-                        System.out.println(deleteId + "번 명언은 존재하지 않습니다.");
+                        System.out.println("%d번 명언은 존재하지 않습니다.".formatted(deleteId));
                     }
-                    break;
-
-                case "수정":
-                    System.out.println("수정할 번호를 입력하세요: ");
-                    int modifyId = sc.nextInt();
-                    sc.nextLine();
+                } catch (NumberFormatException e) {
+                    System.out.println("ID 형식이 올바르지 않습니다. 예: 삭제?id=1");
+                }
+            } else if (command.startsWith("수정?id=")) {
+                try {
+                    int modifyId = Integer.parseInt(command.substring("수정?id=".length()));
 
                     boolean modified = false;
                     for (Quote quote : quotes) {
                         if(quote.id == modifyId) {
-                            System.out.println("기존 명언: " + quote.content);
-                            System.out.println("새 명언을 입력하세요: ");
+                            System.out.println("명언(기존) : " + quote.content);
+                            System.out.print("명언 : ");
                             quote.content = sc.nextLine();
 
-                            System.out.println("기존 작가: " + quote.author);
-                            System.out.println("새 작가를 입력하세요: ");
+                            System.out.println("작가(기존) : " + quote.author);
+                            System.out.print("작가 : ");
                             quote.author = sc.nextLine();
 
-                            System.out.println(modifyId + "번 째 명언이 수정되었습니다");
-                            System.out.println("번호" + quote.id + "\n명언: " + quote.content + "\n작가: " +quote.author);
-
+                            System.out.println("%d번 명언이 수정되었습니다.".formatted(modifyId));
                             modified = true;
                             break;
                         }
                     }
 
                     if(!modified){
-                        System.out.println(modifyId +"는 존재하지 않는 번호입니다.");
+                        System.out.println("%d번 명언은 존재하지 않습니다.".formatted(modifyId));
                     }
-                    break;
-
-                case "종료":
-                    System.out.println("프로그램을 종료합니다.");
-                    return;
-
-                default:
-                    System.out.println("올바른 명령어가 아닙니다.");
+                } catch (NumberFormatException e) {
+                    System.out.println("ID 형식이 올바르지 않습니다. 예: 수정?id=1");
+                }
+            } else if (command.equals("종료")) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else {
+                System.out.println("올바른 명령어가 아닙니다.");
             }
         }
+        sc.close();
     }
 }
-
